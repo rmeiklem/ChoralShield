3172b8d9e32ad1b00090a1f7ba6c9790b55172b9
robbie <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-12-10 11:41:09 +0000

    sprint 1.1

---
+++ Concert.java
@@ -0,0 +1,20 @@
+
+public class Concert
+{
+    private string Custor
+    private int x;
+
+  
+    public Concert()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
