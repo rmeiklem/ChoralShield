9b0958f4d4441038d257df03d30aa6bdafee7f03
Guest User <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-12-10 13:56:02 +0000

    sprint 1.2

--- Concert.java
+++ Concert.java
@@ -1,20 +1,37 @@
 
 public class Concert
 {
-    private string Custor
-    private int x;
+    private String customerid;
+    private int numberoftickets;
+    private char ticketid;
+    private char methodofpurchase;
+    private int seat;
 
-  
     public Concert()
     {
-        // initialise instance variables
-        x = 0;
+        customerid = "";
+        numberoftickets = 0;
+        ticketid = 0;
+        methodofpurchase = 0;
+        seat = 0;
+
+    }
+
+    public void readMemberDetails(String dataItems)
+    {
+
+        
+        String[] rowItems = dataItems.split(",");
+
+       
+        Customerid = rowItems[0];
+        numberoftickets = integer.parseint(rowItems[1]);
+        ticketid = Float.parseFloat(rowItems[3]);
+        memberID = rowItems[4];
+        category = (rowItems[5]).charAt(0);
+
+        bmi = weight/(height*height/10000);
     }
 
     
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
